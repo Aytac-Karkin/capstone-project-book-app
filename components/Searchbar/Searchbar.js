04d50cfcde842950af7fd3833d0c21a843a2470d
Anja Klaßen <anja.klassen@posteo.de>
2024-02-23 12:55:37 +0100

    change Searchbar name

--- components/Searchbar/Searchbar.js
+++ components/Searchbar/Searchbar.js
@@ -13,12 +13,6 @@ export default function SearchBar({ setSearchTerm }) {
   );
 }
 
-const SearchBarContainer = styled.form`
-  display: flex;
-  justify-content: center;
-  margin-bottom: 0.5rem;
-`;
-
 const SearchInput = styled.input`
   padding: 0.3rem;
   font-size: 0.9rem;
